04f7c270dc7a5bd6261f4cf5939b942b2d776d45
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-08-21 19:45:23 -0300

    admin and migrations

--- ifbucks/admin.py
+++ ifbucks/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import ItemPedido
 from .models import Pedidos
-from .models import Pedidos
+from .models import Produtos
 from .models import Usuario
 from .models import TipoPessoa
 from .models import TiposProdutos
 
 admin.site.register(ItemPedido)
 admin.site.register(Pedidos)
+admin.site.register(Produtos)
 admin.site.register(Usuario)
 admin.site.register(TipoPessoa)
 admin.site.register(TiposProdutos)
