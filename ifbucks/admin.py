afc1302fe3ecbb5757064ba224618e6fe634f965
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-09-01 10:53:48 -0300

    changes and comments

--- ifbucks/admin.py
+++ ifbucks/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
-from .models import ItemPedido
+from .models import itemPedido
 from .models import Pedidos
 from .models import Produtos
 from .models import Usuario
 from .models import TipoPessoa
-from .models import TiposProdutos
+from .models import tipoProduto
 
-admin.site.register(ItemPedido)
+admin.site.register(itemPedido)
 admin.site.register(Pedidos)
 admin.site.register(Produtos)
 admin.site.register(Usuario)
 admin.site.register(TipoPessoa)
-admin.site.register(TiposProdutos)
+admin.site.register(tipoProduto)
--- ifbucks/models.py
+++ ifbucks/models.py
@@ -3,79 +3,160 @@ from django.db import models
 from django.db import models
 
 
-class Pedidos(models.Model):
-    mesa = models.IntegerField(default=0)
-    usuario = models.ForeignKey("Usuario", on_delete=models.SET_NULL, null=True)
-    status = models.BooleanField()
-    hora_pedido = models.TimeField()
+# class Pedidos(models.Model):
+#     mesa = models.IntegerField(default=0)
+#     usuario = models.ForeignKey("Usuario", on_delete=models.SET_NULL, null=True)
+#     status = models.BooleanField()
+#     hora_pedido = models.TimeField()
+
+#     class Meta:
+#         constraints = [
+#             models.UniqueConstraint(fields=["id"], name="unique_pedido_id"),
+#         ]
+#         verbose_name = "Pedido"
+#         verbose_name_plural = "Pedidos"
+
+# #  def __str__(self):
+# #        return self.nome quais retornar?
+
+# class Usuario(models.Model):
+#     cpf = models.CharField(max_length=11, unique=True)
+#     tipopessoa = models.ForeignKey("TipoPessoa", on_delete=models.PROTECT)
+#     email = models.EmailField(null=True)  # Note: Should be an email field
+
+#     class Meta:
+#         constraints = [
+#             models.UniqueConstraint(fields=["cpf", "id"], name="unique_usuario_cpf_id"),
+#         ]
+#         verbose_name = "Usuário"
+#         verbose_name_plural = "Usuários"
+
+
+# class Produtos(models.Model):
+#     preco = models.DecimalField(decimal_places=2, max_digits=10)
+#     nome = models.CharField(max_length=250)
+#     descricao = models.CharField(max_length=255, default="1000")
+#     tipoProduto = models.ForeignKey(
+#         "TiposProdutos", on_delete=models.SET_NULL, null=True
+#     )
+#     class Meta:
+#         constraints = [
+#             models.UniqueConstraint(fields=["id"], name="unique_produto_id"),
+#         ]
+#         verbose_name = "Produto"
+#         verbose_name_plural = "Produtos"
+
+# class TiposProdutos(models.Model):
+#     nome = models.CharField(max_length=100)
+#     descricao = models.CharField(max_length=1000)
+
+#     class Meta:
+#         constraints = [
+#             models.UniqueConstraint(fields=["id"], name="unique_tipoproduto_id"),
+#         ]
+#         verbose_name = "Tipo de Produto"
+#         verbose_name_plural = "Tipos de Produtos"
+
+
+# class ItemPedido(models.Model):
+#     pedido = models.ForeignKey("Pedidos", on_delete=models.PROTECT)
+#     produto = models.ForeignKey("Produtos", on_delete=models.PROTECT)
+#     quantidade = models.IntegerField(default=1)
+#     # preco = models.DecimalField(decimal_places=2, max_digits=10) sugestao copilot
+#     # total = models.DecimalField(decimal_places=2, max_digits=10) sugestao copilot
+#     verbose_name = "Item Pedido"
+#     verbose_name_plural = "Itens Pedido"
+
+
+# class TipoPessoa(models.Model):
+#     descricao = models.CharField(max_length=1000)
+
+#     class Meta:
+#         constraints = [
+#             models.UniqueConstraint(fields=["id"], name="unique_tipopessoa_id"),
+#         ]
+#         verbose_name = "Tipo de Pessoa"
+#         verbose_name_plural = "Tipos de Pessoa"
+
+# #usar protect ou cascade?
+
+# refazendo o modelo ------------->
+
+
+class TipoPessoa(models.Model):
+    descricao = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.descricao
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["id"], name="unique_pedido_id"),
-        ]
-        verbose_name = "Pedido"
-        verbose_name_plural = "Pedidos"
+        verbose_name = "Tipo de Pessoa"
+        verbose_name_plural = "Tipos de Pessoa"
 
-#  def __str__(self):
-#        return self.nome quais retornar?
 
 class Usuario(models.Model):
     cpf = models.CharField(max_length=11, unique=True)
-    tipopessoa = models.ForeignKey("TipoPessoa", on_delete=models.PROTECT)
-    email = models.EmailField(null=True)  # Note: Should be an email field
+    tipopessoa = models.ForeignKey(TipoPessoa, on_delete=models.PROTECT)
+    email = models.EmailField(null=True)
+
+    def __str__(self):
+        return self.cpf
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["cpf", "id"], name="unique_usuario_cpf_id"),
-        ]
         verbose_name = "Usuário"
         verbose_name_plural = "Usuários"
+
+
+class Pedidos(models.Model):
+    mesa = models.IntegerField(default=1)
+    usuario_id = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField()
+    hora_pedido = models.TimeField()
+
+    def __str__(self):
+        return f"{self.mesa} - {self.usuario_id} - {self.status} - {self.hora_pedido}"
+
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+
+
+class tipoProduto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Tipo de Produto"
+        verbose_name_plural = "Tipos de Produtos"
 
 
 class Produtos(models.Model):
     preco = models.DecimalField(decimal_places=2, max_digits=10)
     nome = models.CharField(max_length=250)
     descricao = models.CharField(max_length=255, default="1000")
-    tipoProduto = models.ForeignKey(
-        "TiposProdutos", on_delete=models.SET_NULL, null=True
+    tipoProduto_id = models.ForeignKey(
+        tipoProduto, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.nome
+
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["id"], name="unique_produto_id"),
-        ]
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
 
-class TiposProdutos(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=1000)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["id"], name="unique_tipoproduto_id"),
-        ]
-        verbose_name = "Tipo de Produto"
-        verbose_name_plural = "Tipos de Produtos"
-
-
-class ItemPedido(models.Model):
-    pedido = models.ForeignKey("Pedidos", on_delete=models.PROTECT)
-    produto = models.ForeignKey("Produtos", on_delete=models.PROTECT)
+class itemPedido(models.Model):
+    pedido_id = models.ForeignKey(Pedidos, on_delete=models.PROTECT)
+    produto_id = models.ForeignKey(Produtos, on_delete=models.PROTECT)
     quantidade = models.IntegerField(default=1)
-    # preco = models.DecimalField(decimal_places=2, max_digits=10) sugestao copilot
-    # total = models.DecimalField(decimal_places=2, max_digits=10) sugestao copilot
-    verbose_name = "Item Pedido"
-    verbose_name_plural = "Itens Pedido"
 
-
-class TipoPessoa(models.Model):
-    descricao = models.CharField(max_length=1000)
+    def __str__(self):
+        return f"{self.pedido_id} - {self.produto_id} - {self.quantidade}"
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["id"], name="unique_tipopessoa_id"),
-        ]
-        verbose_name = "Tipo de Pessoa"
-        verbose_name_plural = "Tipos de Pessoa"
-
-#usar protect ou cascade?
+        verbose_name = "Item Pedido"
+        verbose_name_plural = "Itens Pedido"
