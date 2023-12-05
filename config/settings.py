a85dcb2f54de302d5646e41b30ec2c6d49a4cafc
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-12-05 15:01:08 -0300

    correção MEDIA_URL

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 STATIC_URL = "/static/"
+MEDIA_ENDPOINT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+FILE_UPLOAD_PERMISSIONS = 0o640
 
 if MODE in ["PRODUCTION", "MIGRATE"]:
     CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
@@ -150,18 +154,4 @@ else:
     MY_IP = os.getenv("MY_IP", "127.0.0.1")
     MEDIA_URL = f"http://{MY_IP}:19003/media/"
 
-# App Uploader settings
-#MEDIA_URL = "http://localhost:8000/media/" 
-MEDIA_URL = "https://ifbucks.1.ie-1.fl0.io/media/" 
-MEDIA_ENDPOINT = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-FILE_UPLOAD_PERMISSIONS = 0o640
-
-
-
-
 CORS_ALLOW_ALL_ORIGINS = True
-
-if MODE == "PRODUCTION":
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
